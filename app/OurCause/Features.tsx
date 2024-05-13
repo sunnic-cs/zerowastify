af4c8cf44b75fe665ce978b27f77ba6cbbb2be73
sunnic-cs <72729142+sunnic-cs@users.noreply.github.com>
2024-05-13 14:31:36 +0800

    Compeleted Our Cause Page
    Moved functions into components
    added new div to containerized content

---
+++ app/OurCause/Features.tsx
@@ -0,0 +1,118 @@
+import React from 'react'
+import Image from 'next/image'
+import ourcauseImage from '../../assets/ourCauseHeader.png'
+import checkImage from '../../assets/Check.png'
+
+const Features = () => {
+  return (
+    <>
+      <div className="py-[48px] lg:py-[84px]">
+        <h1 className="text-[#172026] lg:font-medium text-center lg:text-[42px] text-2xl">
+            Highlights
+        </h1>
+        <p className="pt-[16px] pb-[10px] text-center text-[#36485C]">
+          Key Features
+        </p>
+      </div>
+
+      <div className="flex flex-col gap-y-6 lg:flex-row gap-x-[24px] lg:pl-[131px] lg:pr-[131px]">
+        <div className="w-full rounded-[8px] bg-[#F5F4FF] p-6 flex flex-col lg:justify-between">
+            <div>
+                <h3 className="font-medium text-[#4328EB] text-[18px] lg:text-xl">
+                  Bin Monitoring
+                </h3>
+                <p className="pt-[12px] text-[#36485C] lg:text-[18px]">
+                  Smart Sensors, Smarter Bins
+                </p>
+                <ul className="flex flex-col gap-y-2 pt-4 text-[#5F7896]">
+                  <li className="flex items-center gap-x-2">
+                    <span>
+                      <Image src={checkImage} alt="included" />
+                    </span>
+                    Real-time Monitoring
+                  </li>
+                  
+                  <li className="flex items-center gap-x-2">
+                    <span>
+                      <Image src={checkImage} alt="included" />
+                    </span>
+                    Cost Efficiency
+                  </li>
+                  
+                  <li className="flex items-center gap-x-2">
+                    <span>
+                      <Image src={checkImage} alt="included" />
+                    </span>
+                    Environmental Impact
+                  </li>
+                </ul>
+            </div>
+        </div>
+
+        <div className="w-full rounded-[8px] bg-[#4328EB] p-6 flex flex-col">
+            <div>
+                <h3 className="font-medium text-[#FFFFFF] text-[18px] lg:text-xl lg:justify-between">
+                  Route Optimization
+                </h3>
+                <p className="pt-[12px] text-[#F4F8FA] lg:text-[18px]">
+                  Navigate Wisely
+                </p>
+                <ul className="flex flex-col gap-y-2 pt-4 text-[#F4F8FA]">
+                  <li className="flex items-center gap-x-2">
+                    <span>
+                      <Image src={checkImage} alt="included" />
+                    </span>
+                    Real-time Monitoring
+                  </li>
+                  <li className="flex items-center gap-x-2">
+                    <span>
+                      <Image src={checkImage} alt="included" />
+                    </span>
+                    Cost Efficiency
+                  </li>
+                  <li className="flex items-center gap-x-2">
+                    <span>
+                      <Image src={checkImage} alt="included" />
+                    </span>
+                    Environmental Impact
+                  </li>
+                </ul>
+            </div>
+        </div>
+
+        <div className="w-full rounded-[8px] bg-[#F5F4FF] p-6 flex flex-col">
+            <div>
+                <h3 className="font-medium text-[#4328EB] text-[18px] lg:text-xl">
+                  Data Analytics
+                </h3>
+                <p className="pt-[12px] text-[#36485C] lg:text-[18px]">
+                  Insight that Matter
+                </p>
+                <ul className="flex flex-col gap-y-2 pt-4 text-[#5F7896]">
+                  <li className="flex items-center gap-x-2">
+                    <span>
+                      <Image src={checkImage} alt="included" />
+                    </span>
+                    Predictive Insights
+                  </li>
+                  <li className="flex items-center gap-x-2">
+                    <span>
+                      <Image src={checkImage} alt="included" />
+                    </span>
+                    Performance Monitoring
+                  </li>
+                  <li className="flex items-center gap-x-2">
+                    <span>
+                      <Image src={checkImage} alt="included" />
+                    </span>
+                    Informed Decision Making
+                  </li>
+                </ul>
+            </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Features
---
+++ app/OurCause/Header.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Image from 'next/image'
+import ourcauseImage from '../../assets/ourCauseHeader.png'
+
+const Header = () => {
+  return (
+    <div className="px-4 lg:pt-10 lg:px-1">
+        <div className="px-[20px] lg:px-[280px] lg:flex-row lg:flex">
+          <div className="lg:px-[171px] text-left text-[30px] font-medium text-[#39845A] lg:text-[36px] lg:leading-[52px]">
+            <h1>Introducing 
+              <span className="text-black"> Our</span>
+              <h1>Smart Waste 
+                <span className="text-black"> Management System</span>
+              </h1>
+            </h1>
+            <div className="top-full mt-[25px] border-b-2 border-black w-full"></div>
+            <h3 className="text-[20px] text-black py-[19px]">Home &gt; Our Cause</h3>
+          </div>
+          <Image className="rounded-[22px]" src={ourcauseImage} alt="poster"></Image>
+        </div>
+      </div>
+  )
+}
+
+export default Header
